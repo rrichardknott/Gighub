44bcf78ee2b7aba1e54f1566daec6af907791939
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2020-10-12 10:14:37 -0700

    Created Gig Model

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [NotMapped]
+        [DisplayName("Created")]
+        public string DateTimeString
+        {
+            get
+            {
+                string stringDateTime = DateTime.ToString("MMM dd, yyy");
+                return stringDateTime;
+            }
+        }
+
+    }
+}
