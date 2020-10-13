a7be51a02daba1ea087ef8fb41678a46683b25b2
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2020-10-12 19:20:22 -0700

    Created create view (add a gig form)

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,29 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //public GigsController()
+        //{
+        //    db = new ApplicationDbContext();
+        //}
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = db.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+
+    }
+}
